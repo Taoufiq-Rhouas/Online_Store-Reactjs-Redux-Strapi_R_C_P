9c95d8b3612b862d139b1e1ce802dcc1b23ab247
Taoufiq-Rhouas <rhouastaoufiq@gmail.com>
2023-09-28 22:03:29 +0100

    Redux_Persist_for_save cart_data_in_local_storage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import { store } from './reddux/store.js'
+import { persistor, store } from './reddux/store.js'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={"loadinng..."} persistor={persistor} >
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
--- client/src/reddux/store.js
+++ client/src/reddux/store.js
@@ -1,8 +1,53 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from './cartReducer'
 
+// -----------------
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, cartReducer)
+
 export const store = configureStore({
     reducer: {
-        cart: cartReducer
+        cart: persistedReducer
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 })
+
+// export const store = configureStore({
+//     reducer: {
+//         cart: cartReducer
+//     },
+// })
+
+export let persistor = persistStore(store)
+
+// -----------------
+
+// // S_Comment-1
+// export const store = configureStore({
+//     reducer: {
+//         cart: cartReducer
+//     },
+// })
+// // E_Comment-1
